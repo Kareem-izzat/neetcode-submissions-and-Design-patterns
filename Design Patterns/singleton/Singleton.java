6ddbda1b71d06d2560bebf9f4a46f8e02250eb88
Kareem Al-Qutob <97916953+Kareem-izzat@users.noreply.github.com>
2026-04-12 20:03:20 +0300

    Add files via upload

---
+++ Design Patterns/singleton/Singleton.java
@@ -0,0 +1,11 @@
+public class Singleton {
+    private static Singleton instance;
+    private Singleton() {}
+
+    private static synchronized Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
